82a82a75fc5cd4072da078bc6bb5ce397462dad4
amelv-03 <aiden.melv03@gmail.com>
2024-09-28 00:28:52 +0000

    tweaking idk

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -5,11 +5,11 @@ const Counter = (props) => {
   return (
     <div className='w-100'>
         <h1 className='text-center'>
-            {props.count} Seconds
+          {props.count} Second(s)
         </h1>
     </div>
-  )
-}
+  );
+};
 
 
 export default Counter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Counter from "./counter";
 
 //include images into your bundle
